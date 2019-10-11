9969577ff4d6033a627a272374d3f808dd4d92a6
wlader <wlader.pi@gtmail.com>
2019-10-11 16:33:03 -0300

    CodeWars - Multiplicação simples

---
+++ 8-kyu--Multiply.php
@@ -0,0 +1,21 @@
+<?php
+
+// Solution:
+function multiply($a, $b) {
+  return $a * $b;
+}
+
+ 
+// Sample Tests:
+// PHPUnit Test Examples:
+// TODO: Replace examples and use TDD development by writing your own tests
+class MyTestCases extends TestCase {
+    public function testThatSomethingShouldHappen() {
+      $this->assertEquals("a", "a");
+      $this->assertEquals([0], [0]);
+    }
+}
+
+
+?> 
+ 
